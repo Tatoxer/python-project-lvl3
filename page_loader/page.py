aaf00b7df7bd86ab40bdfa2f5eec3f04705e6673
Anton Selifanov <Tatoxer@gmail.com>
2020-06-05 18:34:17 +0300

    added folder choose

--- page_loader/page.py
+++ page_loader/page.py
@@ -3,17 +3,27 @@ import re
 import requests
 
 
-url = 'https://ru.wikipedia.org'
-folder_name = re.sub(r"https://|http://", "", url)
-folder_name = re.sub(r"\W", "-", folder_name)
-current_directory = os.getcwd()
-try:
-    os.mkdir(folder_name)
-except FileExistsError:
-    print(f"Directory '{folder_name}' is already existing")
+def create_outputs(url, directory=os.getcwd()):
+    folder_name = re.sub(r"https://|http://", "", url)
+    folder_name = re.sub(r"\W", "-", folder_name)
 
-site_path = os.path.join(current_directory, folder_name + "/")
-r = requests.get(url)
+    try:
+        os.mkdir(directory + "/" + folder_name)
+    except FileExistsError:
+        print(f"Directory '{folder_name}' is already existing")
 
-with open(site_path + folder_name + ".html", "w") as f:
-    f.write(r.text)
+    site_path = os.path.join(directory, folder_name + "/")
+    return folder_name, site_path
+
+
+def download_page(url, folder_name, site_path):
+    r = requests.get(url)
+
+    with open(site_path + folder_name + ".html", "w") as f:
+        f.write(r.text)
+    print(f"Page '{folder_name}' was downloaded here: {site_path}")
+
+
+
+# s_dir, f_name, s_path = create_outputs(url)
+# download_page('https://ru.wikipedia.org', f_name, s_path)
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -3,14 +3,20 @@ import argparse
 from page_loader import page
 
 parser = argparse.ArgumentParser(description="Download site page")
-parser.add_argument("URL", type=str, help="URL to download")
+parser.add_argument("url", type=str, help="URL to download")
 parser.add_argument("--output", type=str, help="Path to download web-page. "
-                                                     "Default: current directory")
+                                               "Default: current directory")
 args = parser.parse_args()
 
 
 def main():
-    pass
+    url = args.url
+    if args.output is None:
+        folder_name, site_path = page.create_outputs(url)
+    else:
+        folder_name, site_path = page.create_outputs(url, args.output)
+
+    page.download_page(url, folder_name, site_path)
 
 
 if __name__ == "__main__":
